65038bc2c4ea4305ae067324a13d5ea33c766f40
Tanay0708 <123vyast@gmail.com>
2024-06-18 22:44:27 +0530

    added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
 
 import AllRoutes from './pages/AllRoutes';
 import Home from './pages/Home';
-
-
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
   return (
     <div >
+      <Analytics />
    <AllRoutes />
     </div>
   );
